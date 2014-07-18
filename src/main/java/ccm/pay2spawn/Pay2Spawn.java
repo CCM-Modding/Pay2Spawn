beacd00b3cf311f11a4e6f2f8d19746e6ff85c94
dries007 <admin@dries007.net>
2014-07-18 17:26:28 +0200

    fixed last part of #125

--- src/main/java/ccm/pay2spawn/Pay2Spawn.java
+++ src/main/java/ccm/pay2spawn/Pay2Spawn.java
@@ -47,6 +47,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 import net.minecraft.launchwrapper.Launch;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.ClientCommandHandler;
 import org.apache.logging.log4j.Logger;
 
@@ -117,6 +118,7 @@ public class Pay2Spawn
         {
             e.printStackTrace();
         }
+        Helper.msg(EnumChatFormatting.GREEN + "Reload done!");
     }
 
     public static void reloadDB_Server() throws Exception
